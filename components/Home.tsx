32a4806e07d55c253a3a239db33ba6c61cc72eb5
xImPoSsiBle <ghjklmn1234567890@gmail.com>
2025-10-29 13:06:33 +0500

    first commit

---
+++ components/Home.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+
+const products = [
+  {
+    id: "1",
+    name: "iPhone 15",
+    price: "1200$",
+  },
+  {
+    id: "2",
+    name: "Samsung Galaxy S24",
+    price: "1000$",
+  },
+  {
+    id: "3",
+    name: "MacBook Pro 14",
+    price: "2000$",
+  },
+  {
+    id: "4",
+    name: "Sony WH-1000XM5",
+    price: "350$",
+  }
+]
+
+const Home = () => {
+  return (
+    <View>
+      <Text style={styles.title}>Товары</Text>
+
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text style={styles.price}>{item.price}</Text>
+          </View>
+        )}
+      />
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "500",
+    marginBottom: 10
+  },
+  card: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 12,
+    marginBottom: 10,
+    backgroundColor: "#eee",
+    borderRadius: 10
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 10,
+    marginRight: 15
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "500"
+  },
+  price: {
+    fontSize: 16,
+    color: "green"
+  }
+})
