23c722738a29c43552908ee09bdab6ec84293985
xImPoSsiBle <ghjklmn1234567890@gmail.com>
2025-11-05 13:18:36 +0500

    stlye home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { Redirect } from 'expo-router';
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet } from 'react-native';
 
 const index = () => {
   return <Redirect href="/(tabs)/home" />;
